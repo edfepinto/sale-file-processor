ecf6f0e75b768187da5e6e08cc787c9823348a89
Eduardo Pinto <edfepinto@gmail.com>
2024-01-12 08:39:52 -0300

    feat: create user crud

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_04_202049) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_11_195935) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_04_202049) do
     t.integer "purchase_count"
     t.string "merchant_address"
     t.string "merchant_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
