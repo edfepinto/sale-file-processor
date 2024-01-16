95504317da6f8d38c42d958c28739180cbcefe3b
Eduardo Pinto <edfepinto@gmail.com>
2024-01-16 08:55:00 -0300

    fix: create sale

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,12 @@ Rails.application.routes.draw do
 
   resources :sales do
     collection do
-      get 'upload_form', to: 'sales#upload_form'
-      post 'calculate_balance', to: 'sales#calculate_balance'
-      get 'show_result', to: 'sales#show_result'
-      get 'list_sales', to: 'sales#list_sales'
+      get "upload_form", to: "sales#upload_form"
+      post "calculate_balance", to: "sales#calculate_balance"
+      get "show_result", to: "sales#show_result"
+      get "list_sales", to: "sales#list_sales"
+      get "new", to: "sales#new"
+      post "create_sale", to: "sales#create"
     end
   end
 
